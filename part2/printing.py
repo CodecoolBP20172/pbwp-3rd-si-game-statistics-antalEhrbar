fb9e36639e9a5b2f3e523a24824a54b1a544cfa9
antalEhrbar <ehranti@gmail.com>
2017-07-09 21:41:42 +0200

    final

--- part2/printing.py
+++ part2/printing.py
@@ -1,2 +1,22 @@
 
 # Printing functions
+import sys
+import pprint
+from reports import get_most_played, sum_sold, get_selling_avg, count_longest_title, get_date_avg, get_game, count_grouped_by_genre
+
+file_name = "game_stat.txt"
+year = 2004
+genre = "First-person shooter"
+title = 'Counter-Strike'
+
+a = get_most_played(file_name)
+b = sum_sold(file_name)
+c = get_selling_avg(file_name)
+d = count_longest_title(file_name)
+e = get_date_avg(file_name)
+f = get_game(file_name, title)
+g = count_grouped_by_genre(file_name, genre)
+
+answers_for_Judy = [a, b, c, d, e, f, g]
+
+pprint.pprint(answers_for_Judy) 
--- part2/reports.py
+++ part2/reports.py
@@ -1,2 +1,89 @@
-
+import sys
+import math
+genre = "RPG"
+file_name = "game_stat.txt"
+title = "Counter-Strike"
 # Report functions
+
+
+def get_most_played(file_name):
+    line_in_gam_stat = 0
+    latest_game_year = 0
+    results = []
+    with open(sys.path[0] + '/' + file_name, "r") as myfile:
+        for line in myfile:
+            results.append(line.strip().split('\t'))
+    for i in results:
+        if float(results[line_in_gam_stat][1]) > float(latest_game_year):
+            latest_game_year = results[line_in_gam_stat][1]
+            name_of_this_game = results[line_in_gam_stat][0]
+        line_in_gam_stat += 1
+    return(name_of_this_game)
+
+
+def sum_sold(file_name):
+    line_in_gam_stat = 0
+    num_of_sold_games = 0
+    results = []
+    with open(sys.path[0] + '/' + file_name, "r") as myfile:
+        for line in myfile:
+            results.append(line.strip().split('\t'))
+        for i in results:
+            num_of_sold_games += float(results[line_in_gam_stat][1])
+            line_in_gam_stat += 1
+        return(num_of_sold_games)
+
+
+def get_selling_avg(file_name):
+    line_in_gam_stat = 0
+    num_of_sold_games = 0
+    results = []
+    with open(sys.path[0] + '/' + file_name, "r") as myfile:
+        for line in myfile:
+            results.append(line.strip().split('\t'))
+        for i in results:
+            num_of_sold_games += float(results[line_in_gam_stat][1])
+            line_in_gam_stat += 1
+        return(num_of_sold_games/len(results))
+
+
+def count_longest_title(file_name):
+    len_of_title = 0
+    line_in_gam_stat = 0
+    results = []
+    with open(sys.path[0] + '/' + file_name, "r") as myfile:
+        for line in myfile:
+            results.append(line.strip().split('\t'))
+        for i in results:
+            if len(results[line_in_gam_stat][0]) > len_of_title:
+                len_of_title = len(results[line_in_gam_stat][0])
+            line_in_gam_stat += 1
+        return len_of_title
+
+
+def get_date_avg(file_name):
+    line_in_gam_stat = 0
+    num_of_sold_games = 0
+    results = []
+    with open(sys.path[0] + '/' + file_name, "r") as myfile:
+        for line in myfile:
+            results.append(line.strip().split('\t'))
+        for i in results:
+            num_of_sold_games += float(results[line_in_gam_stat][2])
+            line_in_gam_stat += 1
+        return(math.ceil((num_of_sold_games/len(results))))
+
+
+def get_game(file_name, title):
+    x = 0
+    results = []
+    with open(sys.path[0] + '/' + file_name, "r") as myfile:
+        for line in myfile:
+            results.append(line.strip().split('\t'))
+    for i in results:
+        if results[x][0] == title:
+            break
+        x += 1
+    results[x][1] = float(str(results[x][1]))
+    results[x][2] = int(str(results[x][2]))
+    return results[x]
